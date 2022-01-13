a10392f226a0a838924678a5283b1778a91334d5
Andries Steenkamp <j.andries.j.steenkamp@gmail.com>
2022-01-13 14:07:53 +0100

    some minor refactoring

--- src/MVSK.jl
+++ src/MVSK.jl
@@ -1,85 +1,164 @@
 module MVSK
+using CSV, DataFrames, Dates
 
-using Pkg
-Pkg.status()
+include("mod\\stock_data.jl")   ; using .stock_data ; const sd = stock_data
+include("mod\\data_moments.jl") ; using .data_moments ; const dm = data_moments
+include("mod\\objective_functions.jl") ; using .objective_functions ; const obf = objective_functions
 
-include("src\\SDPmodel.jl")
-include("src\\SDPoptimized.jl")
-using .SDPmodel 
-using .SDPoptimized
+include("opt\\SOS.jl") ; using .SOS
+include("opt\\grid_search.jl") ; using .grid_search  ; const gs = grid_search
+include("opt\\convex_quadratic_optimization.jl") ; using .convex_quadratic_optimization ; const cqo = convex_quadratic_optimization  
 
+export get_MVSK_data,
+       get_MVSK_bounds,
+       save_MVSK
 
-## Running a single model:
-N = Number_of_stocks = 5
-t = Level_of_Hierarchy = 2 # must be higher than 1
-k = 4 # 3 for skewnes, 4 for kurtosis
-mod     = SDPmodel.get_SDP_model(Number_of_stocks,Level_of_Hierarchy,k)
-mod_opt = SDPoptimized.optimize_SDP(mod)
-
-include("src\\stock_data.jl")
-include("src\\moments.jl")
-include("src\\SDPconstraints.jl")
-using .moments 
-using .SDPconstraints 
-using .stock_data
-
-model = Model()
-@variable(model, Lx[moments.make_mon_expo(N,2*t)]) # Create variables
-f = SDPconstraints.make_objective_function(N,k,Lx)
-
-dkm  = moments.make_mon_expo(N,k,isle=false)
-Lxk  = moments.get_Lxᵅ(Lx,dkm)
-R    = stock_data.load_relative_returns_data_matrix([1:N...])
-
-using JuMP
-Primal_status    = string(JuMP.primal_status(mod_opt)) 
-Dual_status      = string(JuMP.dual_status(mod_opt))
-objective_value  = JuMP.objective_value(mod_opt)
-computation_time = JuMP.solve_time(mod_opt)
-
-
-## Running the model for a batch of parameters:
-## Skewness
-Number_of_stocks_list = [5:15 ...]
-Level_of_Hierarchy_list = [2:3 ...] # must be higher than 1
-k = 3 # 3 for skewnes, 4 for kurtosis
-SDPoptimized.batch_optimize_SDP(Number_of_stocks_list, Level_of_Hierarchy_list, k)
-## Kurtosis
-Number_of_stocks_list = [5:15 ...]
-Level_of_Hierarchy_list = [2:3 ...] # must be higher than 1
-k = 4 # 3 for skewnes, 4 for kurtosis
-SDPoptimized.batch_optimize_SDP(Number_of_stocks_list, Level_of_Hierarchy_list, k)
-
-
-
-
-using Test, DataFrames
-using JuMP
-
-
-
-
-mutable struct Bound_data{N,lvl,k}
-    Number_of_stocks::N
-    level::lvl
-    objective::k
+mutable struct MVSK_data
+    n_stocks::Int64
+    Data_matrix::Matrix{Float64}
+    moment_matrices::Dict{String, Matrix{Float64}}
+    #stock_names::Vector{String}
+    # begin_date::Dates.Date
+    # end_date::Dates.Date
 end
-bound_data10_2_3 = Bound_data{10,2,3}
 
+mutable struct MVSK_bounds
+    λ::Vector{Float64}
+    deg_range::StepRange{Int64, Int64}
+    grid_range::Vector{Int64}
+    f₁ₘₐₓ::Dict
+    f₂ₘᵢₙ::Dict
+    f₃ₘₐₓ::Dict
+    f₄ₘᵢₙ::Dict
+    𝒻_tilde::Dict
+end    
 
+###################################################################################
+function get_MVSK_data(n_stocks = 6)
+    Data_matrix = sd.load_cont_comp_returns()[:,1:n_stocks]
+    moment_matrices = Dict( "means_vector"      => dm.get_means_vector(Data_matrix), 
+                            "covariance_matrix" => dm.get_covariance_matrix(Data_matrix),
+                            "skewness_matrix"   => dm.get_skewness_matrix(Data_matrix),
+                            "kurtosis_matrix"   => dm.get_kurtosis_matrix(Data_matrix))
+    
+    MVSK_d = MVSK_data(n_stocks,
+                       Data_matrix,
+                       moment_matrices)
 
-mutable struct Portfolio <: Any # portfolio_candidates
-    number_of_stocks::Int
-    number_of_data_point::Int 
-    stock_tickers::Vector{String} 
-    relative_returns::Matrix{Float64} 
+    return MVSK_d
 end
-ticker_list = get_tickers()
-ticker_list[1:5]
 
-P1 = Portfolio(5,99,["A", "AAL", "AAP", "AAPL", "ABBV"],)
-P1.number_of_stocks
+function get_MVSK_bounds(mod, λ = ones(4)*0.25, deg_range = 4:2:6, grid_range = 2 .^ (1:5))
+    mom_mats = mod.moment_matrices
+    f₁ₘₐₓ = get_f₁ₘₐₓ(mom_mats["means_vector"])
+    f₂ₘᵢₙ = get_f₂ₘᵢₙ(mom_mats["covariance_matrix"])
+    f₃ₘₐₓ = get_f₃ₘₐₓ(mom_mats["skewness_matrix"], deg_range, grid_range)            
+    f₄ₘᵢₙ = get_f₄ₘᵢₙ(mom_mats["kurtosis_matrix"], deg_range, grid_range)
+    𝒻_tilde = get_𝒻_tilde(λ, mom_mats, f₁ₘₐₓ, f₂ₘᵢₙ, f₃ₘₐₓ, f₄ₘᵢₙ, grid_range)
+  
+    MVSK_b =    MVSK_bounds(λ,
+                            deg_range,
+                            grid_range,
+                            f₁ₘₐₓ,f₂ₘᵢₙ,f₃ₘₐₓ,f₄ₘᵢₙ,
+                            𝒻_tilde
+                            )
 
+    return MVSK_b
+end
+
+###################################################################################
+function get_f₁ₘₐₓ(me_vec)
+    f₁ₘₐₓ_val, f₁ₘₐₓ_arg = maximum(me_vec), me_vec .== maximum(me_vec)
+    return Dict("f₁ₘₐₓ_val"=> f₁ₘₐₓ_val,
+                "f₁ₘₐₓ_arg"=> f₁ₘₐₓ_arg)
+end
+
+function get_f₂ₘᵢₙ(cov_mat)
+    f₂ₘᵢₙ_val = cqo.get_variance_min(cov_mat*(10^4))*(10^(-4)) # scaling for stability
+    return Dict("f₂ₘᵢₙ_val"=> f₂ₘᵢₙ_val)
+end
+
+function get_f₃ₘₐₓ(sk_mat, deg_range, grid_range)
+    n_stocks = size(sk_mat)[1] 
+    f₃ = obf.get_f₃(sk_mat) 
+    
+    f₃ₘₐₓ_pop_df = SOS.get_SOS_skewness_estimates(sk_mat, deg_range)
+    f₃ₘₐₓ_gs_df = gs.batch_max(f₃, n_stocks, grid_range)
+    return Dict("SOS_estimates"=> f₃ₘₐₓ_pop_df,
+                "grid_search_estimates"=> f₃ₘₐₓ_gs_df)
+end
+
+function get_f₄ₘᵢₙ(kur_mat, deg_range, grid_range)
+    f₄ = obf.get_f₄(kur_mat)
+    n_stocks = Int(sqrt(size(kur_mat)[1]))
+
+    f₄ₘᵢₙ_cqr = cqo.get_relaxed_kurtosis(kur_mat) 
+    f₄ₘᵢₙ_pop_df = SOS.get_SOS_kurtosis_estimates(kur_mat, deg_range) 
+    f₄ₘᵢₙ_gs_df = gs.batch_min(f₄, n_stocks, grid_range)
+    return Dict("SOS_estimates"=> f₄ₘᵢₙ_pop_df,
+                "grid_search_estimates"=> f₄ₘᵢₙ_gs_df,
+                "convex_quadratic_estimate"=> f₄ₘᵢₙ_cqr)
+end
+
+function get_𝒻_tilde(λ, mom_mats, f₁ₘₐₓ, f₂ₘᵢₙ, f₃ₘₐₓ, f₄ₘᵢₙ, grid_range)
+    n_stocks = length(mom_mats["means_vector"])
+    fₒₚₜ = get_fₒₚₜ(f₁ₘₐₓ, f₂ₘᵢₙ, f₃ₘₐₓ, f₄ₘᵢₙ)
+    𝒻_tilde = obf.get_𝒻(λ, fₒₚₜ, mom_mats)
+    𝒻_tilde_df = grid_search.batch_min(𝒻_tilde, n_stocks, grid_range)
+    return Dict("grid_search_estimates"=> 𝒻_tilde_df)
+end 
+
+function get_fₒₚₜ(f₁ₘₐₓ, f₂ₘᵢₙ, f₃ₘₐₓ, f₄ₘᵢₙ)
+    return (f₁ₘₐₓ["f₁ₘₐₓ_val"],
+            f₂ₘᵢₙ["f₂ₘᵢₙ_val"],
+            f₃ₘₐₓ["SOS_estimates"].obj_val[end],
+            f₄ₘᵢₙ["SOS_estimates"].obj_val[end])
+end
+
+###################################################################################
+function save_MVSK(MVSK_data, MVSK_bounds; delim=',')
+    save_dir = get_save_dir(MVSK_data.n_stocks)
+    mkdir(save_dir)
+    save_MVSK_data(MVSK_data, save_dir)
+    save_MVSK_bounds(MVSK_bounds, save_dir, delim=delim)
+    return save_dir
+end
+
+function save_MVSK_data(MVSK_data, save_dir)
+    data_dir = save_dir*"Data\\"
+    mkdir(data_dir )
+    for mom_mat ∈ ["means_vector", "kurtosis_matrix", "covariance_matrix", "skewness_matrix"]
+        CSV.write(data_dir*mom_mat*".csv", DataFrame(MVSK_data.moment_matrices[mom_mat]),header = false)
+    end
+end
+
+function save_MVSK_parameters(MVSK_bounds, save_dir; delim=',')
+    error("Not coded yet")
+end
+
+function save_MVSK_bounds(MVSK_bounds, save_dir; delim=',')
+    bounds_dir = save_dir*"Bounds\\"
+    mkdir(bounds_dir)
+
+    f₃ₘₐₓ_gs = MVSK_bounds.f₃ₘₐₓ["grid_search_estimates"]
+    f₄ₘᵢₙ_gs = MVSK_bounds.f₄ₘᵢₙ["grid_search_estimates"]
+
+    f₃ₘₐₓ_pop = MVSK_bounds.f₃ₘₐₓ["SOS_estimates"]
+    f₄ₘᵢₙ_pop = MVSK_bounds.f₄ₘᵢₙ["SOS_estimates"]
+
+    𝒻_tilde_gs = MVSK_bounds.𝒻_tilde["grid_search_estimates"]
+
+    CSV.write(bounds_dir*"\\f₃ₘₐₓ_gs.csv", f₃ₘₐₓ_gs, header = true, delim=delim)
+    CSV.write(bounds_dir*"\\f₃ₘₐₓ_pop.csv", f₃ₘₐₓ_pop, header = true, delim=delim)
+
+    CSV.write(bounds_dir*"\\f₄ₘᵢₙ_gs.csv", f₄ₘᵢₙ_gs, header = true, delim=delim)
+    CSV.write(bounds_dir*"\\f₄ₘᵢₙ_pop.csv", f₄ₘᵢₙ_pop, header = true, delim=delim)
+
+    CSV.write(bounds_dir*"\\𝒻_tilde_gs.csv", 𝒻_tilde_gs, header = true, delim=delim)
+end
+
+get_time_now() = replace(string(now()), ":" => "-")
+get_save_dir(n_stocks) = "assets\\"*get_time_now()*"n_stocks_$(n_stocks)\\"
 
 
 end
--- src/sandbox.jl
+++ src/sandbox.jl
@@ -1,73 +1,32 @@
 ## Initialize 
 using Pkg
-Pkg.status()
 Pkg.activate(dirname(@__DIR__))
 
-############################ MODEL ########################################
-include("MODEL\\Model.jl")   ; using .Model 
-mod = build_model(6)
+include("MVSK.jl") ; using .MVSK
 
-# mod.n_stocks
-# mod.Data_matrix
-# mod.moment_matrices
-# mod.f₁ₘₐₓ
-# mod.f₂ₘᵢₙ 
+MVSK_data = MVSK.get_MVSK_data()
+MVSK_bounds = MVSK.get_MVSK_bounds(MVSK_data)
 
-############################## POP ######################################
-me_vec  = mod.moment_matrices["means_vector"]
-cov_mat = mod.moment_matrices["covariance_matrix"]
-sk_mat  = mod.moment_matrices["skewness_matrix"]
-kur_mat = mod.moment_matrices["kurtosis_matrix"]
+print(fieldnames(typeof(MVSK_data)))
+MVSK_data.n_stocks
+MVSK_data.Data_matrix
+MVSK_data.moment_matrices
 
 
-include("POP\\SOS.jl") ; using .SOS
-deg_range = 4:2:8
-# https://jump.dev/SumOfSquares.jl/stable/generated/Polynomial%20Optimization/polynomial_optimization/#The-maxdegree-keyword-argument
+print(fieldnames(typeof(MVSK_bounds)))
+MVSK_bounds.λ
+MVSK_bounds.deg_range
+MVSK_bounds.grid_range
 
-f₃ₘₐₓ_pop_df = SOS.get_SOS_skewness_estimates(sk_mat, deg_range)
-f₄ₘᵢₙ_pop_df = SOS.get_SOS_kurtosis_estimates(kur_mat, deg_range) 
+MVSK_bounds.f₁ₘₐₓ
+MVSK_bounds.f₂ₘᵢₙ
+MVSK_bounds.f₃ₘₐₓ
+MVSK_bounds.f₄ₘᵢₙ
+MVSK_bounds.𝒻_tilde
 
-include("MODEL\\convex_quadratic_optimization.jl")  ; using .convex_quadratic_optimization
-f₄ₘᵢₙ_cqr = convex_quadratic_optimization.get_relaxed_kurtosis(kur_mat)
+MVSK.save_MVSK(MVSK_data, MVSK_bounds)
 
-################################ OBJ FUNCT ####################################
-include("UTIL\\objective_functions.jl"); using .objective_functions
-f₁, f₂, f₃, f₄ = objective_functions.get_f_vec(mod.moment_matrices)
-𝒻 = objective_functions.get_𝒻(ones(4), 
-                              (mod.f₁ₘₐₓ, mod.f₂ₘᵢₙ, f₃ₘₐₓ_pop_df[end,2], abs(f₄ₘᵢₙ_pop_df[end,2])), # fₒₚₜ 
-                              mod.moment_matrices)
-                           
-################################ NON LIN ####################################
-grid_range = 2 .^ (1:6)
-include("NONLIN\\grid_search.jl")  ; using .grid_search 
-gs_upper_bounds = grid_search.batch_min(𝒻, mod.n_stocks, grid_range)
 
-f₃ₘₐₓ_nl_df = grid_search.batch_max(f₃, mod.n_stocks, grid_range)
-f₄ₘᵢₙ_nl_df = grid_search.batch_min(f₄, mod.n_stocks, grid_range)
-
-################################ SAGE ####################################
-# Store data and switch to python:
-using CSV, DataFrames, Dates
-
-function save_data(mod, f₃ₘₐₓ_nl_df, f₄ₘᵢₙ_nl_df, f₃ₘₐₓ_pop_df, f₄ₘᵢₙ_pop_df, gs_upper_bounds; delim=',')
-    save_dir = "assets\\"*replace(string(now()), ":" => "-")
-    mkdir(save_dir)
-    # save moment matrices
-    for mom_mat ∈ ["means_vector", "kurtosis_matrix", "covariance_matrix", "skewness_matrix"]
-        CSV.write(save_dir*"\\"*mom_mat*".csv", DataFrame(mod.moment_matrices[mom_mat]),
-                                            header = false,
-                                            delim=delim)
-    end
-
-    CSV.write(save_dir*"\\f₃ₘₐₓ_nl_df.csv", f₃ₘₐₓ_nl_df, header = true, delim=delim)
-    CSV.write(save_dir*"\\f₄ₘᵢₙ_nl_df.csv", f₄ₘᵢₙ_nl_df, header = true, delim=delim)
-
-    CSV.write(save_dir*"\\f₃ₘₐₓ_pop_df.csv", f₃ₘₐₓ_pop_df, header = true, delim=delim)
-    CSV.write(save_dir*"\\f₄ₘᵢₙ_pop_df.csv", f₄ₘᵢₙ_pop_df, header = true, delim=delim)
-
-    CSV.write(save_dir*"\\gs_upper_bounds.csv", gs_upper_bounds, header = true, delim=delim)
-    return save_dir
-end
 
 save_dir =save_data(mod,
                     f₃ₘₐₓ_nl_df,
@@ -85,6 +44,7 @@ nar = DataFrame(Dict( "f₁ₘₐₓ" => mod.f₁ₘₐₓ,
 
 
 CSV.write(save_dir*"\\parameters.csv", nar, header = true, delim="|")
+
 
 
 ####################################################################
